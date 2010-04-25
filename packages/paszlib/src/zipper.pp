Distrotech/fpc

commit 1a7cc68f442506e2b0d20a6b22bb1c328dfa4bc0

* Fixed TZipFileEntry.Assign and added convenience call for TZipper.ZipFiles
git-svn-id: http://svn.freepascal.org/svn/fpc/trunk@13464 3ad0048d-3df7-0310-abae-a5850022a9f2

packages/paszlib/src/zipper.pp
@@ -332,7 +332,9 @@ Type
     Destructor Destroy;override;
     Procedure ZipAllFiles; virtual;
     Procedure ZipFiles(AFileName : String; FileList : TStrings);
+    Procedure ZipFiles(FileList : TStrings);
     Procedure ZipFiles(AFileName : String; Entries : TZipFileEntries);
+    Procedure ZipFiles(Entries : TZipFileEntries);
     Procedure Clear;
   Public
     Property BufferSize : LongWord Read FBufSize Write SetBufSize;
@@ -1401,14 +1403,23 @@ end;
 Procedure TZipper.ZipFiles(AFileName : String; FileList : TStrings);
 
 begin
-  FFiles.Assign(FileList);
   FFileName:=AFileName;
+end;
+
+procedure TZipper.ZipFiles(FileList: TStrings);
+begin
+  FFiles.Assign(FileList);
   ZipAllFiles;
 end;
 
 procedure TZipper.ZipFiles(AFileName: String; Entries: TZipFileEntries);
 begin
   FFileName:=AFileName;
+  ZipFiles(Entries);
+end;
+
+procedure TZipper.ZipFiles(Entries: TZipFileEntries);
+begin
   FEntries.Assign(Entries);
   ZipAllFiles;
 end;
@@ -1901,6 +1912,8 @@ begin
     FSize:=Z.FSize;
     FDateTime:=Z.FDateTime;
     FStream:=Z.FStream;
+    FOS:=Z.OS;
+    FAttributes:=Z.Attributes;
     end
   else
     inherited Assign(Source);

commit 03158ed1c2281ce15733f4e4ee86fe4406b9df0a

* TZipper.ZipFiles should zip files, got broken in r13464
git-svn-id: http://svn.freepascal.org/svn/fpc/trunk@15176 3ad0048d-3df7-0310-abae-a5850022a9f2

packages/paszlib/src/zipper.pp
@@ -1459,6 +1459,7 @@ Procedure TZipper.ZipFiles(AFileName : String; FileList : TStrings);
 
 begin
   FFileName:=AFileName;
+  ZipFiles(FileList);
 end;
 
 procedure TZipper.ZipFiles(FileList: TStrings);